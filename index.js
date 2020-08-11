7dd6eb19528eb2e605ef33bd5a93bd729013a60d
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2020-08-11 16:30:07 -0300

    Banco de dados e mais

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const database = require('./database')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -9,6 +10,28 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) =>{
     res.render('index')
+})
+
+app.post('/criminoso', (req, res) =>{
+    let autor = req.body.autor
+    let criminoso = req.body.criminoso
+    let descricao = req.body.descricao
+
+    let dados = {
+        nome: criminoso,
+        descricao: descricao,
+        autor: autor
+    }
+
+    if(autor != undefined && criminoso != undefined){
+        database.insert(dados).table('criminosos').then(dados =>{
+            res.sendStatus(200)
+            res.redirect('/')
+        }).catch(err =>{
+            res.sendStatus(400)
+            console.log(err)
+        })
+    }
 })
 
 
